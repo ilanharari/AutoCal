ece789f32e1f1513e147ed9c8c23f97de8869687
Ilan Harari <ilanhrr99@gmail.com>
2020-04-06 15:25:51 -0700

    Initial Commit

---
+++ AutoCal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoCal
+//
+//  Created by Ilan Harari on 4/6/20.
+//  Copyright © 2020 Ilan Harari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
